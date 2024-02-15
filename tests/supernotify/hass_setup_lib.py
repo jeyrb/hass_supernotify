b5af7c453d58ed352ad03c895c4d741aac67bc1b
Jey Burrows <jrb@jeymail.net>
2024-02-15 23:25:41 +0000

    test handling

--- tests/supernotify/hass_setup_lib.py
+++ tests/supernotify/hass_setup_lib.py
@@ -1,6 +1,10 @@
+'''
+Test fixture support
+'''
 
 from homeassistant import config_entries
-
+import logging
+_LOGGER = logging.getLogger(__name__)
 
 def register_mobile_app(hass,
                         device_registry,
@@ -13,9 +17,12 @@ def register_mobile_app(hass,
                         title="Test Device"):
     config_entry = config_entries.ConfigEntry(domain=domain, data={
     }, version=1, minor_version=0.1, title=title, source=source)
-    hass.config_entries._entries[config_entry.entry_id] = config_entry
-    hass.config_entries._domain_index.setdefault(
-        config_entry.domain, []).append(config_entry)
+    try:
+        hass.config_entries._entries[config_entry.entry_id] = config_entry
+        hass.config_entries._domain_index.setdefault(
+            config_entry.domain, []).append(config_entry)
+    except Exception as e:
+        _LOGGER.warn("Unable to mess with HASS config entries for mobile app faking: %s", e)
 
     device_entry = device_registry.async_get_or_create(
         config_entry_id=config_entry.entry_id,
